df98073faecd54ccfdf26f2e65a4b418730b290b
SP-Upreti <surendraupreti441@gmail.com>
2024-12-21 21:54:10 +0545

    Update useLogin.tsx

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -23,7 +23,7 @@ export default function useLogin() {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password }),
-                credentials: "include"
+                credentials: "include", // Include this only if required
             });
             const res = await req.json();
 
